8ec45c5e368791a1b5dc9b892e8b0fd78de2aaae
PacasProGit <mpacaspro@cpsaa.com.pe>
2024-01-07 23:25:07 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Ruta principal con parámetros
+  { path: 'birthday', component: AppComponent },
+
+  // Ruta predeterminada (puede redirigir a una página específica si lo deseas)
+  { path: '**', redirectTo: 'birthday' }, //predefinir los textos
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
